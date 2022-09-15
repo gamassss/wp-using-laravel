2489dc5ee7856985fe12247e557a2255c3acfafa
gamassss <gamassakbar@gmail.com>
2022-09-15 18:44:34 +0700

    overwrite routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PriController;
 use App\Http\Controllers\PrjController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PasienController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
